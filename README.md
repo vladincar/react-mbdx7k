6caf972d115238d501259ae146d5c916b0fd9feb
vladincar <vladincar@gmail.com>
2021-10-16 22:02:14 +0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# react-mbdx7k
+
+[Edit on StackBlitz ⚡️](https://stackblitz.com/edit/react-mbdx7k)
---
+++ src/Square.js
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Square = () => {
+  return <button className="square">"pop"</button>;
+};
+
+export default Square;
